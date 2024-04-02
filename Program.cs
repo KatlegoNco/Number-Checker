3f899c0af143d524be478467878d6200ea8595e2
Grealish10k <katlegoncongwane20@gmail.com>
2024-04-02 14:40:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PrimeNumberCheckerApp
+{
+    public class Program
+    {
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false; //Numbers less than or equal to 1 are not prime
+            }
+
+            //Check divisibility up to the square root of the number
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false; //Not a prime number
+                }
+            }
+
+            return true; //It's a prime number
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (IsPrime(inputNumber))
+            {
+                Console.WriteLine($"{inputNumber} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{inputNumber} is not a prime number.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
